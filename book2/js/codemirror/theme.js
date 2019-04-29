a170ab123a7798fdc6073bb75ed9445a6516da4d
Gregor Anzelj <gregor.anzelj@gmail.com>
2019-04-29 11:26:19 +0200

    Deleted unecessary files

--- book2/js/codemirror/theme.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('#theme').change(function() {
-	    if ($(this).prop('checked') == true) {
-	        editor.setOption('theme', 'rin-dark');
-	    } else {
-	        editor.setOption('theme', 'rin-light');
-	    }
-    })
-})
